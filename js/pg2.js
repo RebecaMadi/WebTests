768822682840f5cde8bda43d378b1230914903a2
Rebeca Oliveira <rebecamady23@gmail.com>
2023-05-15 10:52:10 -0400

    3.0

--- js/pg2.js
+++ js/pg2.js
@@ -7,3 +7,75 @@ title.textContent = 'Diretórios e Arquivos';
 
 let p1 = createP(dmain);
 p1.textContent = 'Muitos usuários têm dificuldades com o GNU/Linux porque não têm uma visão geral sobre o que está guardado em que local. Neste capítulo, mostramos um pouco da organização dos arquivos do GNU/Linux.';
+
+let p2 = createP(dmain);
+p2.textContent = `Atualmente, existem excelentes interfaces gr´aficas muito amig´aveis que permitem realizar todo o tipo de opera¸c˜ao em Linux, mas ainda assim ´e muito
+importante ter dom´ınio sobre o ambiente CLI (Command Line Interface)
+que ´e uma interface na qual comandos e respostas s˜ao transmitidos em forma
+de texto. Shell ´e a classe de programas respons´avel por implementar a linguagem desta interface (exemplo: bash, dash, csh, zsh), e terminal ´e a classe
+de programas que traz uma interface gr´afica ao shell. Dentre os programas
+terminal, alguns dos mais conhecidos s˜ao: gnome-terminal, xterm, konsole.
+“ls”, “man” (ver se¸c˜ao 1.2), “fpc”, “alomamae” (ap´os compilado) s˜ao
+exemplos de comandos que s˜ao executados em shell. Normalmente, a parte
+que o usu´ario vˆe do shell ´e o prompt de comandos, representado por “$ >”.
+Para executar um terminal dentro dos laborat´orios do DInf, basta navegar nos menus existentes na parte superior da tela e dentro de “Aplica¸c˜oes”
+estar´a o programa chamado “Terminal”.
+Para facilitar a utiliza¸c˜ao do terminal, repare que as setas cima e baixo
+no teclado podem ser utilizadas para navegar entre os ´ultimos comandos
+digitados; e, ao pressionar TAB duas vezes consecutivas, o terminal muitas
+vezes pode lhe oferecer op¸c˜oes de como auto-completar nomes de comandos
+e nomes de arquivos.`;
+
+let p3 = createP(dmain);
+p3.textContent = `´Atualmente, existem excelentes interfaces gr´aficas muito amig´aveis que permitem realizar todo o tipo de opera¸c˜ao em Linux, mas ainda assim ´e muito
+importante ter dom´ınio sobre o ambiente CLI (Command Line Interface)
+que ´e uma interface na qual comandos e respostas s˜ao transmitidos em forma
+de texto. Shell ´e a classe de programas respons´avel por implementar a linguagem desta interface (exemplo: bash, dash, csh, zsh), e terminal ´e a classe
+de programas que traz uma interface gr´afica ao shell. Dentre os programas
+terminal, alguns dos mais conhecidos s˜ao: gnome-terminal, xterm, konsole.
+“ls”, “man” (ver se¸c˜ao 1.2), “fpc”, “alomamae” (ap´os compilado) s˜ao
+exemplos de comandos que s˜ao executados em shell. Normalmente, a parte
+que o usu´ario vˆe do shell ´e o prompt de comandos, representado por “$ >”.
+Para executar um terminal dentro dos laborat´orios do DInf, basta navegar nos menus existentes na parte superior da tela e dentro de “Aplica¸c˜oes”
+estar´a o programa chamado “Terminal”.
+Para facilitar a utiliza¸c˜ao do terminal, repare que as setas cima e baixo
+no teclado podem ser utilizadas para navegar entre os ´ultimos comandos
+digitados; e, ao pressionar TAB duas vezes consecutivas, o terminal muitas
+vezes pode lhe oferecer op¸c˜oes de como auto-completar nomes de comandos
+e nomes de arquivos.`;
+
+let p4 = createP(dmain);
+p4.textContent = `´Atualmente, existem excelentes interfaces gr´aficas muito amig´aveis que permitem realizar todo o tipo de opera¸c˜ao em Linux, mas ainda assim ´e muito
+importante ter dom´ınio sobre o ambiente CLI (Command Line Interface)
+que ´e uma interface na qual comandos e respostas s˜ao transmitidos em forma
+de texto. Shell ´e a classe de programas respons´avel por implementar a linguagem desta interface (exemplo: bash, dash, csh, zsh), e terminal ´e a classe
+de programas que traz uma interface gr´afica ao shell. Dentre os programas
+terminal, alguns dos mais conhecidos s˜ao: gnome-terminal, xterm, konsole.
+“ls”, “man” (ver se¸c˜ao 1.2), “fpc”, “alomamae” (ap´os compilado) s˜ao
+exemplos de comandos que s˜ao executados em shell. Normalmente, a parte
+que o usu´ario vˆe do shell ´e o prompt de comandos, representado por “$ >”.
+Para executar um terminal dentro dos laborat´orios do DInf, basta navegar nos menus existentes na parte superior da tela e dentro de “Aplica¸c˜oes”
+estar´a o programa chamado “Terminal”.
+Para facilitar a utiliza¸c˜ao do terminal, repare que as setas cima e baixo
+no teclado podem ser utilizadas para navegar entre os ´ultimos comandos
+digitados; e, ao pressionar TAB duas vezes consecutivas, o terminal muitas
+vezes pode lhe oferecer op¸c˜oes de como auto-completar nomes de comandos
+e nomes de arquivos.`;
+
+let p5 = createP(dmain);
+p5.textContent = `´Atualmente, existem excelentes interfaces gr´aficas muito amig´aveis que permitem realizar todo o tipo de opera¸c˜ao em Linux, mas ainda assim ´e muito
+importante ter dom´ınio sobre o ambiente CLI (Command Line Interface)
+que ´e uma interface na qual comandos e respostas s˜ao transmitidos em forma
+de texto. Shell ´e a classe de programas respons´avel por implementar a linguagem desta interface (exemplo: bash, dash, csh, zsh), e terminal ´e a classe
+de programas que traz uma interface gr´afica ao shell. Dentre os programas
+terminal, alguns dos mais conhecidos s˜ao: gnome-terminal, xterm, konsole.
+“ls”, “man” (ver se¸c˜ao 1.2), “fpc”, “alomamae” (ap´os compilado) s˜ao
+exemplos de comandos que s˜ao executados em shell. Normalmente, a parte
+que o usu´ario vˆe do shell ´e o prompt de comandos, representado por “$ >”.
+Para executar um terminal dentro dos laborat´orios do DInf, basta navegar nos menus existentes na parte superior da tela e dentro de “Aplica¸c˜oes”
+estar´a o programa chamado “Terminal”.
+Para facilitar a utiliza¸c˜ao do terminal, repare que as setas cima e baixo
+no teclado podem ser utilizadas para navegar entre os ´ultimos comandos
+digitados; e, ao pressionar TAB duas vezes consecutivas, o terminal muitas
+vezes pode lhe oferecer op¸c˜oes de como auto-completar nomes de comandos
+e nomes de arquivos.`;
